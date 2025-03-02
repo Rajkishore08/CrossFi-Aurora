bd29d23fd8262e565be43eae9c5891fada8af00d
Raj Kishore S <127388780+Rajkishore08@users.noreply.github.com>
2025-03-02 15:51:26 +0530

    Update DeFiAgent.sol

--- contracts/DeFiAgent.sol
+++ contracts/DeFiAgent.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DeFiAgent {
+    IERC20 public frax;
+    address public owner;
+
+    constructor(address _frax) {
+        frax = IERC20(_frax);
+        owner = msg.sender;
+    }
+
+    function buyFrax(uint256 amount) external {
+        require(msg.sender == owner, "Not authorized");
+        frax.transferFrom(owner, address(this), amount);
+    }
+
+    function sellFrax(uint256 amount) external {
+        require(msg.sender == owner, "Not authorized");
+        frax.transfer(owner, amount);
+    }
+}
